ab5876d5a6b1f3579edd0d5a207894dcab929f1d
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-10-26 02:14:42 -0400

    starting to add get routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,8 @@
 //include two routes
 
 //First: A GET Route to /survey which should display the survey page.
-
+app.get("/survey", function(req,res){
+    console.log("this worked")
+})
 
 //Second: A default, catch-all route that leads to home.html which displays the home page.
