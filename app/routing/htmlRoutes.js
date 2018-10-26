82fb46267919cd5773bdf578cbc4dbd9cac58518
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-10-26 01:37:44 -0400

    more setup

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,6 @@
+//include two routes
+
+//First: A GET Route to /survey which should display the survey page.
+
+
+//Second: A default, catch-all route that leads to home.html which displays the home page.
