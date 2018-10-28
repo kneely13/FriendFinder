942dbd9d53e1fa3dfbac4e25c5bd13b931e83cf8
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-10-28 14:06:39 -0400

    change

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,32 +8,3 @@ router.get('/api', function (req, res) {
 })
 
 module.exports = router
-//build out nice list of api routes  then return it
-
-
-
-//include two routes
-
-//First: A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// app.get("/api/friends", function(req, res) {
-//     return res.json(friends); //this function will need to display a JSON of all possible friends
-// });
-
-  
-// //Second: A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic
-
-// app.post("/api/friends", function (req, res){
-    
-
-//     var newFriend = req.body;
-
-//     console.log(newFriend);
-
-//     friends.push(newFriend); 
-
-//     res.json(newFriend);
-// })
-
-// app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
